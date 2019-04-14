3ae5bd40fe11892e7f40d1e850b63a08d7fc89f9
MAJU STUDENT <majustudent@jinnah.edu>
2019-04-14 16:14:03 +0500

    This is my third commit

--- ExamsApp/App.js
+++ ExamsApp/App.js
@@ -27,6 +27,11 @@ export default class AnatomyExample extends Component {
             Obtain a challenging leadership position applying creative problem solving and lean management skills with a growing company to achieve optimum utilization of its resources and maximum profits
           </Text>
 
+          <Text>
+            Experience:
+            Fresh Graduate
+          </Text>
+
 
 
 
